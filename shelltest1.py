88182fc66d818545d1a249a5df766d7c38de5933
Steven Loe <steven.loe@gmail.com>
2016-12-31 18:59:09 -0700

    test shell commands

---
+++ shelltest1.py
@@ -0,0 +1,13 @@
+import time
+from subprocess import call
+
+print("Starting TrackMouse")
+print("Start GPS Daemon and wait 3 seconds...")
+
+call(["sudo", "gpsd", "/dev/ttyUSB0", "-F", "/var/run/gpsd.sock"])
+# sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
+
+time.sleep(3)
+print("finished!")
+
+call(["cgps", "-s"])
